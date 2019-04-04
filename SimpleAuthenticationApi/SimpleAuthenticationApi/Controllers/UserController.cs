446b4da7a42fc103e97c7518e23cc2c33b0b7ed6
FSOFT.FPT.VN/ThienBV2 <fsoft.com.vn>
2019-04-04 14:02:03 +0700

    first upload

---
+++ SimpleAuthenticationApi/SimpleAuthenticationApi/Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SimpleAuthenticationApi.Dto;
+using SimpleAuthenticationApi.Entity;
+using SimpleAuthenticationApi.Helpers;
+using SimpleAuthenticationApi.Service;
+
+namespace SimpleAuthenticationApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public UserController(
+            IUserService userService,
+            IMapper mapper,
+            IOptions<AppSettings> appSettings)
+        {
+            _userService = userService;
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+        }
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]UserDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+            try
+            {
+                _userService.Create(user, userDto.Password);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]UserDto userDto)
+        {
+            var user = _userService.Authenticate(userDto.Username, userDto.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get()
+        {
+                return Ok(_userService.GetAll());
+        }
+    }
+}
---
+++ SimpleAuthenticationApi/SimpleAuthenticationApi/Service/UserService.cs
@@ -0,0 +1,177 @@
+﻿using SimpleAuthenticationApi.Entity;
+using SimpleAuthenticationApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleAuthenticationApi.Service
+{
+    public class UserService : IUserService
+    {
+        private DataContext _context;
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// verify login, return user info if login correct
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public User Authenticate(string username, string password)
+        {
+            //check null or empty username and password
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            var user = _context.Users.Where(u=>u.Username==username).SingleOrDefault();
+            //check user existence
+            if (user == null)
+            {
+                return null;
+            }
+            //verify password
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+            return user;
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users;
+        }
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+        /// <summary>
+        /// register new user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public User Create(User user, string password)
+        {
+ 
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new Exception("Password is required");
+            }               
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new Exception("Username is required");
+            }
+            if (_context.Users.Any(x => x.Username == user.Username))
+            {
+                throw new Exception("Username : " + user.Username + " is already taken");
+            }
+            // create encoding password
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            //save new user into database
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+        public void Update(User userParam, string password = null)
+        {
+            var user = _context.Users.Find(userParam.Id);
+
+            if (user == null)
+                throw new Exception("User not found");
+
+            if (userParam.Username != user.Username)
+            {
+                if (_context.Users.Any(x => x.Username == userParam.Username))
+                    throw new Exception("Username " + userParam.Username + " is already taken");
+            }
+            user.FirstName = userParam.FirstName;
+            user.LastName = userParam.LastName;
+            user.Username = userParam.Username;
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+            }
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// encoding password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            }
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        /// <summary>
+        /// verify password when login
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="storedHash"></param>
+        /// <param name="storedSalt"></param>
+        /// <returns></returns>
+        private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            }
+            if (storedHash.Length != 64)
+            {
+                throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            }
+            if (storedSalt.Length != 128)
+            {
+                throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+            } 
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i])
+                    {
+                        return false;
+                    } 
+                }
+            }
+            return true;
+        }
+    }
+}
+
