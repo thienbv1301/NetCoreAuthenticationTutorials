c660c21d77b6a34c409c7ce295f4d40f08bb9e8a
thienbv <thienbv.1301@gmail.com>
2019-04-05 10:07:51 +0700

    upload source code

---
+++ SimpleRoleBaseAuthenticationApi/SimpleAuthenticationApi/AutoMapper/AutoMapperProfile .cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SimpleAuthenticationApi.Dto;
+using SimpleAuthenticationApi.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAuthenticationApi.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+    
+}
---
+++ SimpleRoleBaseAuthenticationApi/SimpleAuthenticationApi/Helpers/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAuthenticationApi.Helpers
+{
+    public static class Role
+    {
+        public const string Admin = "ADMIN";
+        public const string User = "USER";
+        public const string AdminOrUser = Admin + "," + User;
+    }
+}
